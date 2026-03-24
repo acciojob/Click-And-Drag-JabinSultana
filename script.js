0a82cf7fe2f5bd41c5cde35ac969c5494869cb40
JabinSultana <jabinasiff9926@gmail.com>
2026-03-24 18:54:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 const slider = document.querySelector('.items');
 
 let isDown = false;
-let startX = 0;
-let scrollLeft = 0;
+let startX;
+let scrollLeft;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
 
-  startX = e.pageX;
+  slider.classList.add('active');
+
+  startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
-});
-
-slider.addEventListener('mousemove', (e) => {
-  if (!isDown) return;
-
-  e.preventDefault();
-
-  const walk = startX - e.pageX;
-
-  // FORCE scroll update
-  slider.scrollLeft = scrollLeft + walk;
-});
-
-slider.addEventListener('mouseup', () => {
-  isDown = false;
 });
 
 slider.addEventListener('mouseleave', () => {
   isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+
+  if (!isDown) return;
+
+  e.preventDefault();
+
+  const x = e.pageX - slider.offsetLeft;
+
+  const walk = (x - startX) * 2; // IMPORTANT multiplier
+
+  slider.scrollLeft = scrollLeft - walk;
+
 });
